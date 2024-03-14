3d6d7f288c22943596d22097fc394c22f654f42b
William <mauritius.william.wijaya1@gmail.com>
2024-03-14 20:23:06 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+//* CASE
+
+// CAMEL CASE
+let apaMauKu = 32
+
+// KEBAB CASE
+// let coba-lagi = 32
+
+// SNAKE CASE
+let apa_mau_ku = 32
+
+// PASCAL CASE
+let ApaMauKu = 32
+
+
+
+//* INCREMENT && DECREMENT
+let age = 40
+age += 3
+age--
+
+// console.log(age);
+
+//* OPERASI DI STRING
+let food = 'banana ' + 'banana'
+// console.log('banana ' + 'banana');
+
+//* AND (&&) & OR (||)
+const city = 'GOTHAMe'
+if (city !== 'GOTHAM' || city === 'GOTHAM') {
+    console.log('mantap');
+}
+
+// perbandingan outputnya adalah boolean
